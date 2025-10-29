cbe7577c03b44769f7bbe88033f25d0ed9897efd
OhMinGeun9609 <oksk199428@gmail.com>
2025-10-29 19:07:05 +0900

    feat : 스프라이트 설정 변경, 슬라이스

--- Assets/Scripts/Entity/BaseController.cs
+++ Assets/Scripts/Entity/BaseController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer playerSr;
+
+    private Vector2 moveDirection = Vector2.zero;
+    public Vector2 MoveDirection { get { return moveDirection; } }
+
+    private Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    private Rigidbody2D _rigidbody;
+    private AnimaionHandler anime;
+
+    protected virtual void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        anime = GetComponent<AnimaionHandler>();
+    }
+
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
         
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        
+    }
+
+    protected virtual void Action()
+    {
+
+    }
+
+    private void Moving(Vector2 direction)
+    {
+
     }
 }
