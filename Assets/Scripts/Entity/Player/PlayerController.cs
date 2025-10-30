2f4e21ba1855cb3faaebef8e55476570495141fe
OhMinGeun9609 <oksk199428@gmail.com>
2025-10-30 19:50:49 +0900

    Update PlayerController.cs

--- Assets/Scripts/Entity/Player/PlayerController.cs
+++ Assets/Scripts/Entity/Player/PlayerController.cs
@@ -43,32 +43,4 @@ public class PlayerController : BaseController
 
         return nearestTarget;
     }
-
-    protected override void HandleAction()
-    {
-        base.HandleAction();
-
-public class PlayerController : BaseController
-{
-    protected override void Start()
-    {
-        base.Start();
-    }
-
-    protected override void Action()
-    {
-        float horizon = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        moveDirection = new Vector2(horizon, vertical);
-
-        if(moveDirection.magnitude != 0f)
-        {
-            moveDirection = moveDirection.normalized;
-        }
-        else
-        {
-            moveDirection = Vector2.zero;
-        }
-    }
 }
