2dcdbf01136f7ba9385a0496bce02a2bea3ba57d
qkrtkdgus010208 <psh010208@naver.com>
2025-10-30 21:01:14 +0900

    fear: 카메라 관련 오브젝트들 프리팹화

---
+++ Assets/Scripts/Camera/CameraConfinerSetter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraConfinerSetter : MonoBehaviour
+{
+    [SerializeField] private CinemachineConfiner2D confinerComponent;
+
+    [SerializeField] private GameObject boundingAreaObject;
+
+    void Start()
+    {
+        SetConfinerBoundingShape();
+    }
+
+    private void SetConfinerBoundingShape()
+    {
+        if (confinerComponent == null)
+        {
+            Debug.LogError("Confiner가 존재하지 않습니다.");
+            return;
+        }
+
+        if (boundingAreaObject == null)
+        {
+            Debug.LogError("Bounding Area Object가 존재하지 않습니다.");
+            return;
+        }
+        
+        PolygonCollider2D boundaryCollider = boundingAreaObject.GetComponent<PolygonCollider2D>();
+
+        if (boundaryCollider == null)
+        {
+            Debug.LogError("Bounding Area Object가 PolygonCollider2D component를 가지고 있지 않습니다.");
+            return;
+        }
+
+        confinerComponent.m_BoundingShape2D = boundaryCollider;
+
+        confinerComponent.InvalidateCache();
+
+        Debug.Log("Cinemachine Confiner 할당 완료!");
+    }
+}
