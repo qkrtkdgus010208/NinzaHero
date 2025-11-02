cc54c09a7e8d0cdd06f7e716d6e8315f4c3407b0
qkrtkdgus010208 <psh010208@naver.com>
2025-11-02 20:05:48 +0900

    feat: Attack 애니메이션 추가

--- Assets/Scripts/Entity/AnimationHandler.cs
+++ Assets/Scripts/Entity/AnimationHandler.cs
@@ -2,9 +2,13 @@
 
 public class AnimationHandler : MonoBehaviour
 {
-    private static readonly int IsMoving = Animator.StringToHash("IsMove");
+    private static readonly int IsMove = Animator.StringToHash("IsMove");
     private static readonly int MoveX = Animator.StringToHash("MoveX");
     private static readonly int MoveY = Animator.StringToHash("MoveY");
+
+    private static readonly int IsAttack = Animator.StringToHash("IsAttack");
+    private static readonly int AttackX = Animator.StringToHash("AttackX");
+    private static readonly int AttackY = Animator.StringToHash("AttackY");
 
     private Animator animator;
 
@@ -15,11 +19,20 @@ public class AnimationHandler : MonoBehaviour
 
     public void Move(Vector2 obj)
     {
-        bool isMoving = obj.magnitude > 0.5f;
+        bool isMove = obj.magnitude > 0.5f;
         Vector2 normalized = obj.normalized;
 
-        animator.SetBool(IsMoving, isMoving);
         animator.SetFloat(MoveX, normalized.x);
         animator.SetFloat(MoveY, normalized.y);
+        animator.SetBool(IsMove, isMove);
+    }
+
+    public void Attack(Vector2 obj)
+    {
+        Vector2 normalized = obj.normalized;
+
+        animator.SetFloat(AttackX, normalized.x);
+        animator.SetFloat(AttackY, normalized.y);
+        animator.SetTrigger(IsAttack);
     }
 }
