176e963e798fdeef9aeb2f6b31023948d3cb5b13
songduckhee <vhaqjvl1@gmail.com>
2025-11-04 21:03:24 +0900

    refector: uimanager수정

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -5,47 +5,46 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-  public GameObject HpBarUI; //캔버스 안에 UI들 배치해놓는 순서
-  public GameObject StageMove;
-  public GameObject SkillSlot;
-  public GameObject GameClear;
-  public GameObject GameOver;
+	public GameObject HpBarUI; //캔버스 안에 UI들 배치해놓는 순서
+	public GameObject StageMove;
+	public GameObject SkillSlot;
+	public GameObject GameClear;
+	public GameObject GameOver;
+	public GameObject BossHealth;
 
 
-  
 
-  private void Awake()
-  {
-	GameOver.SetActive(false);
-	GameClear.SetActive(false);
-	SkillSlot.SetActive(false);
-	HpBarUI.SetActive(true);
-	
-	
-  }
+	private void Awake()
+	{
+		GameOver.SetActive(false);
+		GameClear.SetActive(false);
+		SkillSlot.SetActive(false);
+		HpBarUI.SetActive(true);
+		BossHealth.SetActive(false);
+	}
 
 
-  public void ShowGameOver()
-  {
-	GameOver.SetActive(true);
-  }
+	public void ShowGameOver()
+	{
+		GameOver.SetActive(true);
+	}
 
-  public void ShowStageMove()
-  {
-	StageMove.SetActive(true);
-  }
+	public void ShowStageMove()
+	{
+		StageMove.SetActive(true);
+	}
 
-  public void ShowSkillSlot()
-  {
-	SkillSlot.SetActive(true);
-  }
+	public void ShowSkillSlot()
+	{
+		SkillSlot.SetActive(true);
+	}
 
-  public void ShowGameClear()
-  {
-	GameClear.SetActive(true);
-  }
-  public void ShowHpBarUI()
-  {
-	HpBarUI.SetActive(true);
-  }
+	public void ShowGameClear()
+	{
+		GameClear.SetActive(true);
+	}
+	public void ShowHpBarUI()
+	{
+		HpBarUI.SetActive(true);
+	}
 }
